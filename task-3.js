21d03909228663b4bb8d0a0e46aee110470a0ea7
RomanOmelianenko <r_omelianenko@ukr.net>
2020-08-14 22:46:43 +0300

    HW-5

--- task-3.js
+++ task-3.js
@@ -1,36 +1,36 @@
 class Storage {
-    constructor(items) {
-        this.items = items;
-    }
+  constructor(items) {
+    this.items = items;
+  }
 
-    getItems() {
-        return this.items;
-    }
+  getItems() {
+    return this.items;
+  }
 
-    addItem(item) {
-        this.items.push(item);
-    }
+  addItem(item) {
+    this.items.push(item);
+  }
 
-    removeItem(item) {
-        let index = this.items.indexOf(item);
-        if (index != -1) {
-            this.items.splice(index, 1);
-        }
+  removeItem(item) {
+    let index = this.items.indexOf(item);
+    if (index != -1) {
+      this.items.splice(index, 1);
     }
+  }
 }
 
 const storage = new Storage([
-    "Нанитоиды",
-    "Пролонгер",
-    "Железные жупи",
-    "Антигравитатор"
+  "Нанитоиды",
+  "Пролонгер",
+  "Железные жупи",
+  "Антигравитатор",
 ]);
 
 const items = storage.getItems();
-console.log(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
 
 storage.addItem("Дроид");
-console.log(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
 
 storage.removeItem("Пролонгер");
-console.log(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
